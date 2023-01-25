9ba51771989000ce7a0943d87a2883110ab5bec1
Camille Basbous <camillebasbous@gmail.com>
2023-01-25 13:41:34 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Comp from './Comp';
 
 function App() {
+  const data = [
+   {  key:"1Wnq",
+      list:"first",
+      value : "A",
+      child : { 
+        key: "5Xzs",
+        list:"first",
+        value : "E",
+      }
+    },
+    { key:"2Fmc",
+      list:"first",
+      value : "B",
+      child: { 
+        key: "6Nbk",
+        list:"first",
+        value : "F",
+      }
+    },
+    { key: "3Rgl",
+      list:"first",
+      value : "C",
+      child:{ 
+        key: "7Ohv",
+        list:"first",
+        value : "G",
+      }
+    },
+    { key: "4Syu",
+      list:"first",
+      value : "D",
+      child: { 
+        key: "8Ypo",
+        list:"first",
+        value : "H",
+      }
+    },
+    { key: "1Lnq",
+      list:"second",
+      value : "A",
+      child:{ 
+      key: "5Ipp",
+      list:"second",
+      value : "E",
+    }
+    },
+    { key: "2Jhh",
+      list:"second",
+      value : "B",
+      child:{ 
+      key: "6Kjh",
+      list:"second",
+      value : "F",
+    }
+    },
+    { key: "3Ffg",
+      list:"second",
+      value : "C",
+      child: { 
+      key: "7Lbc",
+      list:"second",
+      value : "G",
+    }
+    },
+    { key: "4Xcc" ,
+      list:"second",
+      value : "D",
+      child: { 
+      key: "8Igd",
+      list:"second",
+      value : "H",
+    }
+    },
+
+  ]
+ 
+
+//
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Comp 
+      data = {data}
+      />
     </div>
   );
 }
---
+++ src/Comp.js
@@ -0,0 +1,101 @@
+import { useEffect, useMemo, useState } from "react";
+import Tooltip from "./Tooltip";
+
+function Comp({data}) {
+
+const [randomCalc, setRandomCalcl] = useState(Math.random() * (100 -1) + 1)
+const [complexCalcResult, setComplexCalcResult] = useState(null)
+
+
+useEffect(()=>{
+    setComplexCalcResult(Math.sqrt(randomCalc * 80 / 7 ))
+},[randomCalc])
+
+    return (
+      <div className="comp" >
+       <ul>
+      {
+       data.map((el,i)=>
+           el.list === "first" && 
+                <>
+                
+                <li 
+                key={i} 
+                >
+                <Tooltip 
+                content={data.filter(filtEl=> el.value === filtEl.value).map((mapEl)=>mapEl.key)} direction="top">
+                  {el.value}
+                  </Tooltip>
+                  
+                  <ul>
+                  {
+                      el.child && 
+                 
+                            <li>
+                                <Tooltip  
+                                content={data.filter(filtEl=> el.child.value === filtEl.child.value).map((mapEl)=>mapEl.child.key)} direction="top">
+                                {el.child.value}
+                                </Tooltip>
+                            </li>
+                        
+                        
+                    }
+                
+                  </ul>
+                </li>
+                </>
+        
+        
+        )
+  
+      }
+            </ul>
+
+            <ul>
+            {
+       data.map((el,i)=>
+           el.list === "second" && 
+                <>
+                
+                <li 
+                key={i} 
+                >
+                <Tooltip 
+                content={data.filter(filtEl=> el.value === filtEl.value).map((mapEl)=>mapEl.key)} direction="top">
+                  {el.value}
+                  </Tooltip>
+                  
+                  <ul>
+                    {
+                      el.child && 
+                 
+                            <li>
+                                <Tooltip  
+                                content={data.filter(filtEl=> el.child.value === filtEl.child.value).map((mapEl)=>mapEl.child.key)} direction="top">
+                                {el.child.value}
+                                </Tooltip>
+                            </li>
+                        
+                        
+                    }
+                
+                  </ul>
+                </li>
+                </>
+        
+        
+        )
+  
+      }
+            </ul>
+            {complexCalcResult}
+        <button onClick={()=>{
+            setRandomCalcl(Math.random() * (100 -1) + 1)
+        }}>
+            click me
+        </button>
+      </div>
+    );
+  }
+  
+  export default Comp;
